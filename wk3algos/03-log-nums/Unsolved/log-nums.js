1bed292125c0548fe96626ffcebd4a9ed72b6409
Andy Humes <andrew.humes@du.edu>
2023-05-06 14:13:22 -0600

    solve

--- wk3algos/03-log-nums/Unsolved/log-nums.js
+++ wk3algos/03-log-nums/Unsolved/log-nums.js
@@ -1,4 +1,31 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
+var logNums = function(num) {
+    for (var i = 1; i <= num; i++) {
+        console.log(i)
+    }
+};
+//  //
+ 
+// Sure, the for loop is a control flow statement that allows you to execute a block of code repeatedly based on a specified condition.
+
+// In the code you provided, the for loop has three parts separated by semicolons:
+
+// The initialization: var i = 1; This part sets up the starting value of the loop counter i, which is typically set to the initial value of the loop. In this case, the loop starts at 1.
+
+// The condition: i <= num; This part specifies the condition that must be true in order for the loop to continue executing. In this case, the loop will continue to execute as long as i is less than or equal to the value of num.
+
+// The increment: i++ This part specifies the action to take after each iteration of the loop. In this case, i is incremented by 1 after each iteration.
+
+// So, in summary, the for loop in this code initializes a loop counter i to 1, checks if it's less than or equal to the value of num, and increments the loop counter by 1 after each iteration. This loop will continue to execute until the loop counter i is no longer less than or equal to num.
+
+
+var logNums = function(num) {
+    for (var i = 1; i <= num; i++) {
+        console.log(i)
+    }
+}
+
+
+
