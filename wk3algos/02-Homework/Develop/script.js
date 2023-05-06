b4f14caaa85890164b701d75b32b2560e9e217c7
Andy Humes <andrew.humes@du.edu>
2023-05-06 16:30:00 -0600

    beginlogic

--- wk3algos/02-Homework/Develop/script.js
+++ wk3algos/02-Homework/Develop/script.js
@@ -7,13 +7,23 @@ var passwordBtn = document.querySelector("#password");
 // Write password to the #password input
 function writePassword() {
 let quantity = window.prompt("How many characters?", "8 minimum");
+let quantityChars = Number(quantity);
+console.log(quantityChars);
+ // now have I saved my quantity as a number, rather than the default string //
 if (quantity < 8 || quantity > 128) {
   window.alert("Incorrect characters");
-  console.log(Number(quantity));
   return
 }
 
+let special = window.prompt("How many special characters?", 0);
+let quantitySpec = Number(special);
+console.log(quantitySpec);
 
+
+
+
+//above will be the password generation logic// 
+// below is where you render pasword to the page //
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
